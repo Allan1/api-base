8c9c925e619b6abae3de4b0aa3cb5d886f03f6f9
Allan <allanoliveira.main@gmail.com>
2018-06-06 23:51:06 -0300

    fixes eslint errors

--- common/mixins/kue.js
+++ common/mixins/kue.js
@@ -1,42 +1,42 @@
+'use strict';
+
 module.exports = function(Model, options) {
-  'use strict';
   var app = require('../../server/server');
   var loopback = require('loopback');
   var kue = require('kue');
   var moment = require('moment');
   require('dotenv').config();
-  
+
   var queue = kue.createQueue({
-	  // prefix: 'q',
-	  redis: {
-	    port: process.env.REDIS_PORT,
-	    host: process.env.REDIS_DOMAIN,
-	    auth: process.env.REDIS_PASSWORD,
-	    options: {
-	      // see https://github.com/mranney/node_redis#rediscreateclient
-	    }
-	  }
-	});
+    // prefix: 'q',
+    redis: {
+      port: process.env.REDIS_PORT,
+      host: process.env.REDIS_DOMAIN,
+      auth: process.env.REDIS_PASSWORD,
+      options: {
+        // see https://github.com/mranney/node_redis#rediscreateclient
+      },
+    },
+  });
 
-	Model.clearJobs = function () {
-		
-	};
+  Model.clearJobs = function() {
 
-	Model.queueEmail = function (data) {
-		var job = queue.create('email',data).save();
-	}
+  };
 
-	Model.sendEmail = function (data,cb) {
-	  app.models.Email.send({
-	    to: data.to,
-	    from: data.from,
-	    subject: data.subject,
-	    html: data.html
-	  },cb);
-	};
+  Model.queueEmail = function(data) {
+    var job = queue.create('email', data).save();
+  };
 
-	queue.process('email', function(job, done){
-	  Model.sendEmail(job.data, done);
-	});
+  Model.sendEmail = function(data, cb) {
+    app.models.Email.send({
+      to: data.to,
+      from: data.from,
+      subject: data.subject,
+      html: data.html,
+    }, cb);
+  };
 
-}
+  queue.process('email', function(job, done) {
+    Model.sendEmail(job.data, done);
+  });
+};
--- tests/accounts.test.js
+++ tests/accounts.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const request = require('supertest');
 
 // jest.mock('../../app/photo_model');
@@ -5,31 +7,30 @@ const app = require('../server/server');
 
 describe('GET /api/accounts', () => {
   afterEach(() => {
-    //app.server.close();
+    // app.server.close();
   });
 
   test('should respond with a 401 for non authenticated request', () => {
     return request(app)
       .get('/api/accounts')
-      //.expect('Content-Type', /html/)
-      .expect(401)
-      /*.then(response => {
+      // .expect('Content-Type', /html/)
+      .expect(401);
+      /* .then(response => {
         expect(response.text).toMatch(
           /<title>Express App Testing Demo<\/title>/
         );
-      });*/
+      }); */
   });
 });
 
 describe('POST /api/accounts', () => {
-
   test('should respond with a 200 for valid params', () => {
     return request(app)
       .post('/api/accounts')
       .send({
         name: 'foo',
         email: 'foo@bar.com',
-        password: '123'
+        password: '123',
       })
       .expect(200);
   });
